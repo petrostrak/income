569fe1917ab74307b3cac58b038cce8e39afccc9
petrostrak <pit.trak@gmail.com>
2024-02-10 18:20:14 +0200

    Add logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 slint::include_modules!();
 
+const TAX_PER: f64 = 0.30;
+const OWNER_PER: f64 = 0.55;
+const PROFIT_PER: f64 = 0.05;
+const OPERATIONAL_EXPENSES_PER: f64 = 0.10;
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
+    let ui_handle = ui.as_weak();
+    ui.on_divide_income(move |s| {
+        let ui = ui_handle.unwrap();
+        let num = s.parse::<f64>().map_err(|e| e.to_string()).unwrap();
+
+        let tax = num * TAX_PER;
+        let owner = num * OWNER_PER;
+        let profit = num * PROFIT_PER;
+        let op_ex = num * OPERATIONAL_EXPENSES_PER;
+
+        let result = format!(
+            "Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOp.Ex: {:.2}\n",
+            tax, owner, profit, op_ex
+        );
+
+        ui.set_results(result.into());
+    });
 
     ui.run()
 }
